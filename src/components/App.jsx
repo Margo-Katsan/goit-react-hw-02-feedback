b26b8810dc458204b4135fa45223bc27ac379c0a
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2023-08-13 01:44:37 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
 import { Section } from "./Section/Section";
+import css from "./App.module.css"
 
 export class App extends Component {
   state = {
@@ -9,6 +10,7 @@ export class App extends Component {
     neutral: 0,
     bad: 0
   }
+
   countTotalFeedback() {
     return this.state.good + this.state.neutral + this.state.bad;
   }
@@ -26,14 +28,20 @@ export class App extends Component {
     })
   }
   render() {
+    const { good, neutral, bad } = this.state;
+
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback}/>
         </Section>
-        <Section title="Statistics">
-          <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
-        </Section>
+        {good === 0 && neutral === 0 && bad === 0 ? (
+          <p className={css.message}>There is no feedback</p>
+        ) : (
+          <Section title="Statistics">
+            <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
+          </Section>
+        )}
       </>
     );     
   }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,12 @@
+import { nanoid } from 'nanoid'
 import css from "./FeedbackOptions.module.css"
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div className={css.options}>
-      <button className={css.option} onClick={() => {onLeaveFeedback(options[0])}}>{options[0]}</button>
-      <button className={css.option} onClick={() => {onLeaveFeedback(options[1])}}>{options[1]}</button>
-      <button className={css.option} onClick={() => {onLeaveFeedback(options[2])}}>{options[2]}</button>
+      {options.map(option => (
+        <button key={nanoid()} className={css.option} onClick={() => {onLeaveFeedback(option)}}>{option}</button>
+      ))}
     </div>
   );
 }
